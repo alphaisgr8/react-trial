1b4c3b1027da09a71ef65f2dae905e361ca7f0f1
alphaisgr8 <alphaisgr8@gmail.com>
2020-04-09 17:20:32 +0530

    Updates

---
+++ precache-manifest.9ad8792ce3a9200fefdfee267ecd62e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4f478c658c7a1324d868635c8502ff",
+    "url": "/react-trial/index.html"
+  },
+  {
+    "revision": "3ca3f6fe75b573e514bc",
+    "url": "/react-trial/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d1cb9e89e8a3856257a9",
+    "url": "/react-trial/static/js/2.62260934.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-trial/static/js/2.62260934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca3f6fe75b573e514bc",
+    "url": "/react-trial/static/js/main.615359c1.chunk.js"
+  },
+  {
+    "revision": "695a5f6490d2c57922a6",
+    "url": "/react-trial/static/js/runtime-main.49f1ff1c.js"
+  }
+]);
